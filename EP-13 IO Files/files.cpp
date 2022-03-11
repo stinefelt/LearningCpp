387b2594b5f6f6d0a69066d8d17cd942d585b1e6
Jack Jones <j.jones@stinefelt.com>
2022-03-11 10:46:17 -0500

    FILE IO

--- EP-13 IO Files/files.cpp
+++ EP-13 IO Files/files.cpp
@@ -1,35 +1,64 @@
-//READ FILE
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #define COL_WIDTH 80
 
+// Variables
+char filename[FILENAME_MAX + 1];
+char input_line[COL_WIDTH + 1];
+int c; // Input Character
+
+// Function Declerations
+void WriteFile(std::ofstream& fileOut, std::string content, int numb);
+void ReadFile(std::ifstream& fileIn);
+bool bCanAccessFile(std::ofstream& fileOut);
+bool bCanAccessFile(std::ifstream& fileIn);
+
 int main()
 {
-	int c; // input character
-	char filename[FILENAME_MAX];
-	char input_line[COL_WIDTH + 1];
-
-	std::cout << "Enter a file name and press ENTER: ";
+	// Ask the user for the filename
+	std::cout << "Enter filename and press ENTER: " << std::endl;
+	// Get the user input for the filename
 	std::cin.getline(filename, FILENAME_MAX);
+	// Create our ofstream file object and referenc the filename that we received from the user.
+	std::ofstream file_out(filename);
+	// Can we acces let's write content to the file	
+	WriteFile(file_out, "Hello first line of text", 0);
+	WriteFile(file_out, "Hello second line of text", 0);
+	WriteFile(file_out, "Hello third line of text", 0);
 
-	std::ifstream file_in(filename);
-
-	// Can we open the file?
-	if (!file_in)
+	for (int i = 0; i <= 100; i++)
 	{
-		std::cout << filename << " couldn't be open!" << std::endl;
-		return -1;
+		WriteFile(file_out, "Line Number: ", i);
+	}
+
+	// Close the file
+	file_out.close();
+	// Let user know that we are good.
+	std::cout << "File write was seccessful!" << std::endl;
+	// Create our read file object
+	std::ifstream fileRead(filename);
+	ReadFile(fileRead);
+}
+
+void ReadFile(std::ifstream& fileIn)
+{
+	if (!fileIn) return;
+	if (!bCanAccessFile(fileIn))
+	{
+		std::cout << "Can't access the file error." << std::endl;
+		return;
 	}
 
 	while (true)
 	{
-		for (int i = 1; i <= 24 && !file_in.eof(); ++i)
+		for (int i = 1; i <= 24 && !fileIn.eof(); ++i)
 		{
-			file_in.getline(input_line, COL_WIDTH);
+			fileIn.getline(input_line, COL_WIDTH);
 			std::cout << input_line << std::endl;
 		}
-		if (file_in.eof())
+		if (fileIn.eof())
 		{
 			break;
 		}
@@ -41,65 +70,33 @@ int main()
 			break;
 		}
 	}
-	return 0;
 }
 
-// WRITE FILE
-//#include <iostream>
-//#include <fstream>
-//#include <string>
-//
-//// Variables
-//char filename[FILENAME_MAX + 1];
-//
-//// Function Declerations
-//void WriteFile(std::ofstream& fileOut, std::string content);
-//bool bCanAccessFile(std::ofstream& fileOut);
-//
-//int main()
-//{
-//	// Ask the user for the filename to write.
-//	std::cout << "Enter file name and press ENTER: ";
-//	// Get the user input for the filename.
-//	std::cin.getline(filename, FILENAME_MAX);
-//	// Create our ofstream file object and reference the filename that we received from the user.
-//	std::ofstream file_out(filename);
-//	// We can access let's write content to the file.
-//	WriteFile(file_out, "Hello first line of text.");
-//	WriteFile(file_out, "Hello second line of text.");
-//	WriteFile(file_out, "Hello third line of text.");
-//	// Close the file out once we are done.
-//	file_out.close();
-//	// Let user know we are good to go.
-//	std::cout << "File write was successful!" << std::endl;
-//}
-//
-///// <summary>
-///// Takes in a ofstream file and writes the string contents.
-///// </summary>
-///// <param name="fileOut">ofstream filename</param>
-///// <param name="content">string content</param>
-//void WriteFile(std::ofstream& fileOut, std::string content)
-//{
-//	if (!fileOut) return;
-//	if (!bCanAccessFile(fileOut))
-//	{
-//		std::cout << "Can't access the file error." << std::endl;
-//		return;
-//	}
-//	fileOut << content << std::endl;
-//}
-//
-///// <summary>
-///// Returns true of the file can be opened.
-///// </summary>
-///// <param name="fileOut">bool true/false</param>
-///// <returns>True if file can be opened, false if not.</returns>
-//bool bCanAccessFile(std::ofstream& fileOut)
-//{
-//	if (!fileOut)
-//	{
-//		return false; // Can't open file
-//	}
-//	return true;
-//}
+void WriteFile(std::ofstream& fileOut, std::string content, int numb)
+{
+	if (!fileOut) return;
+	if (!bCanAccessFile(fileOut))
+	{
+		std::cout << "Can't access the file error." << std::endl;
+		return;
+	}
+	fileOut << content << numb << std::endl;
+}
+
+bool bCanAccessFile(std::ifstream& fileIn)
+{
+	if (!fileIn)
+	{
+		return false; // Can't open the file
+	}
+	return true;
+}
+
+bool bCanAccessFile(std::ofstream& fileOut)
+{
+	if (!fileOut)
+	{
+		return false; // Can't open the file
+	}
+	return true;
+}
